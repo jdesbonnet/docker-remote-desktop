d0927faf8475d49c6a8874bed9093e511823300a
Joe Desbonnet <jdesbonnet@gmail.com>
2024-11-06 21:37:31 +0000

    script to download dev tools

---
+++ download_dev_tools.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Script to download IDEs etc.
+
+wget https://ftp.snt.utwente.nl/pub/software/eclipse/technology/epp/downloads/release/2024-06/R/eclipse-jee-2024-06-R-linux-gtk-x86_64.tar.gz
+wget https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.24.4-stable.tar.xz
+
+
+
