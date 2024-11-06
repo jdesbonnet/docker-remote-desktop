e2906ef1c6108c2df7411b9f9db27ef4420bda2e
Joe Desbonnet <jdesbonnet@gmail.com>
2024-11-06 21:49:49 +0000

    Android command line tools

--- download_dev_tools.sh
+++ download_dev_tools.sh
@@ -3,6 +3,7 @@
 
 wget https://ftp.snt.utwente.nl/pub/software/eclipse/technology/epp/downloads/release/2024-06/R/eclipse-jee-2024-06-R-linux-gtk-x86_64.tar.gz
 wget https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.24.4-stable.tar.xz
+wget https://dl.google.com/android/repository/commandlinetools-linux-11076708_latest.zip
 
 
 
